0cb1d33738520c82a7b730003bdf4c28fe4924fd
mantaionut <ionut@janeasystems.com>
2023-07-31 12:46:29 +0300

    Fix tests

--- .circleci/scripts/binary_windows_test.sh
+++ .circleci/scripts/binary_windows_test.sh
@@ -6,14 +6,11 @@ source "${BINARY_ENV_FILE:-/c/w/env}"
 export CUDA_VERSION="${DESIRED_CUDA/cu/}"
 export VC_YEAR=2019
 
+pushd "$BUILDER_ROOT"
 if [[ ${BUILD_SHARED_LIBS:-true} == "false" ]]; then
     ./windows/internal/static_lib_test.bat
 else
     ./windows/internal/smoke_test.bat
 fi
-
-pushd "$BUILDER_ROOT"
-
-
 
 popd
